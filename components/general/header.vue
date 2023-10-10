c2c1f22ebe321f399522652bc8679ac4efe6c0d1
Lukenoutte <lukenoutte@gmail.com>
2023-10-10 10:57:00 -0300

    feature/layouts

---
+++ components/general/header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="h-10">
+    Header
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <GeneralHeader/>
+    <NuxtPage/>
+  </div>
+</template>
