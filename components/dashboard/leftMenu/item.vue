5766d0b364a9e9a96ecb67f9ea8e102be9324672
Lukenoutte <lukenoutte@gmail.com>
2023-10-27 23:11:38 -0300

    feature/dashboard layout

--- components/dashboard/leftMenu/item.vue
+++ components/dashboard/leftMenu/item.vue
@@ -2,10 +2,10 @@
   <div :class="{ 'bg-primary': isSelected }"
     class="cursor-pointer h-10 mt-2 px-3 flex justify-center items-center rounded-xl"
   >
-    <div class="w-2/5 flex items-center text-gray-400 min-w-[130px]"
+    <div class="flex items-center text-gray-400"
       :class="{ 'text-white': isSelected, 'hover:text-primary': !isSelected }">
       <UIcon class="text-2xl mr-4" :name="icon" />
-      <span class="text-sm">{{ title }}</span>
+      <span class="text-sm min-w-[70px]">{{ title }}</span>
     </div>
   </div>
 </template>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,12 +1,8 @@
 <template>
-  <div class="mt-5 flex flex-row">
-    <DashboardLeftMenu class="hidden lg:block"/>
-    <div class="lg:pl-16 w-full px-5 lg:mt-2">
-      <UCard>{{ mobileMenuIsOpen }}</UCard>
-    </div>
-  </div>
+  <UCard>{{ mobileMenuIsOpen }}</UCard>
 </template>
 
 <script setup>
+  definePageMeta({ layout: 'dashboard' })
   const { mobileMenuIsOpen } = useGlobalStore();
 </script>
