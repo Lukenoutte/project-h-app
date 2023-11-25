009631dc4ad763772876c3de7964109cd971f083
Lukenoutte <lukenoutte@gmail.com>
2023-11-25 11:20:32 -0300

    feature/404 page

---
+++ error.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="h-screen flex flex-col justify-between">
+        <GeneralHeader />
+        <div class="flex-1 overflow-y-auto flex justify-center items-center">
+            <div>
+                <div class="flex flex-col items-center text-gray-300">
+                    <div class="flex justify-center">
+                        <strong class="text-8xl mr-2">404</strong>
+                        <UIcon class="text-8xl" name="i-heroicons-face-frown" />
+                    </div>
+                    <strong class="mt-2 text-2xl">{{ $t('page-not-found') }}</strong>
+                </div>
+                <NuxtLink to="/" class="flex justify-center">
+                    <UButton class="mt-8" :title="$t('continue')" :label="$t('continue')" />
+                </NuxtLink>
+            </div>
+        </div>
+        <GeneralFooter />
+    </div>
+</template>
+
+<script setup></script>
